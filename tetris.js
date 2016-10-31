12789fa2d9ec65dc0eb561fdb93a3a500a5c6d12
brianbyrne <brian.byrne@zonal.co.uk>
2016-10-31 20:18:59 +0000

    complet with bug in rotation

--- tetris.js
+++ tetris.js
@@ -1,5 +1,5 @@
 // tetris tutorial video: https://www.youtube.com/watch?v=H2aW5V46khA
-// 41:32
+// 45:40
 
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
@@ -8,6 +8,7 @@ context.scale(20, 20);
 const matrix = [];
 
 function arenaSweep() {
+	let rowCount = 1;
 	outer: for (let y = arena.length -1; y > 0; --y) {
 		for (let x = 0; x < arena[y].length; ++x) {
 			if (arena[y][x] === 0) {
@@ -18,6 +19,9 @@ function arenaSweep() {
 		let row = arena.splice(y, 1)[0].fill(0);
 		arena.unshift(row);
 		++y;
+		
+		player.score += rowCount * 10;
+		rowCount *= 2;
 	}
 }
 
@@ -125,6 +129,7 @@ function playerDrop() {
 		playerReset();
 		player.pos.y = 0;
 		arenaSweep();
+		updateScore();
 	}
 	dropCounter = 0;
 }
@@ -146,6 +151,8 @@ function playerReset() {
 	
 	if (collide(arena, player)) {
 		arena.forEach(row => row.fill(0));
+		player.score = 0;
+		updateScore();
 	}
 }
 
@@ -205,6 +212,10 @@ function update(time = 0) {
 	requestAnimationFrame(update);
 }
 
+function updateScore() {
+	document.getElementById('score').innerText = player.score;
+}
+
 const colours = [
 	null,
 	'green',
@@ -218,8 +229,9 @@ const colours = [
 const arena = createMatrix(12, 20);
 
 const player = {
-	pos: { x: 5, y: 5 },
-	matrix: createPiece('T')
+	pos: { x: 0, y: 0  },
+	matrix: null,
+	score: 0
 }
 
 document.addEventListener('keydown', event => {
@@ -240,4 +252,5 @@ document.addEventListener('keydown', event => {
 	
 });
 
+playerReset();
 update();
